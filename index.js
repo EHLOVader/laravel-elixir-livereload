355129c6f40cfddd45feb7ef87ea647450babf3b
Joseph Richardson <EHLOVader@users.noreply.github.com>
2015-03-07 22:26:35 -0500

    Updated index.js with main elixer extend command for livereload.

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-var through = require("through2"),
-	gutil = require("gulp-util");
+var gulp = require('gulp'),
+    elixir = require('laravel-elixir'),
+    utilities = require('laravel-elixir/ingredients/commands/Utilities'),
+    notifications = require('laravel-elixir/ingredients/commands/Notification'),
+    livereload = require('gulp-livereload');
 
-module.exports = function (param) {
-	"use strict";
+elixir.extend('livereload', function(src, output) {
 
-	// if necessary check for required param(s), e.g. options hash, etc.
-	if (!param) {
-		throw new gutil.PluginError("gulp-laravel-elixir-livereload", "No param supplied");
-	}
+    gulp.task('livereload', function() {
+        livereload.listen();
+        gulp.on('stop', function(){
+            livereload();
+        });
+    });
 
-	// see "Writing a plugin"
-	// https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/README.md
-	function laravelElixirLivereload(file, enc, callback) {
-		/*jshint validthis:true*/
+    this.registerWatcher('livereload');
 
-		// Do nothing if no contents
-		if (file.isNull()) {
-			this.push(file);
-			return callback();
-		}
+    return this.queueTask('livereload');
 
-		if (file.isStream()) {
-
-			// http://nodejs.org/api/stream.html
-			// http://nodejs.org/api/child_process.html
-			// https://github.com/dominictarr/event-stream
-
-			// accepting streams is optional
-			this.emit("error",
-				new gutil.PluginError("gulp-laravel-elixir-livereload", "Stream content is not supported"));
-			return callback();
-		}
-
-		// check if file.contents is a `Buffer`
-		if (file.isBuffer()) {
-
-			// manipulate buffer in some way
-			// http://nodejs.org/api/buffer.html
-			file.contents = new Buffer(String(file.contents) + "\n" + param);
-
-			this.push(file);
-
-		}
-		return callback();
-	}
-
-	return through.obj(laravelElixirLivereload);
-};
+});
